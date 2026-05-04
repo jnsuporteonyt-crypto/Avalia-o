9f2bfbd489aaa004bc334d8692bb8e97eebf0619
jnsuporteonyt-crypto <jnsuporteonyt@gmail.com>
2026-05-04 17:32:24 -0300

    Update and rename sw (1).js to sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'aq-v2';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => {
+        if (k !== CACHE) return caches.delete(k);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request).catch(() => caches.match('./index.html'));
+    })
+  );
+});
